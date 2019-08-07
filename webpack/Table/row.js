cbd14a256ad9ad1107b3437d92a6fa035c9e5098
pedro823 <grey.pedrinho@gmail.com>
2019-08-07 19:18:30 +0100

    Returns empty line when title & presenter are empty

--- webpack/Table/row.js
+++ webpack/Table/row.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Row = ({ presenter, title }) => (
-    <div class="home--table-row">
-        <div class="home--table-cell">{ title }</div>
-        <div class="home--table-cell">{ presenter }</div>
-    </div>
-)
+const Row = ({ presenter, title }) => {
+    if (!presenter && !title) {
+        return <span></span>
+    }
+    return (
+        <div class="home--table-row">
+            <div class="home--table-cell">{ title }</div>
+            <div class="home--table-cell">{ presenter }</div>
+        </div>
+    )
+}
 
 export default Row

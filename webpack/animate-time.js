fc685409a67f19a8dd20b434a54f5c2fbf3b9ba9
pedro823 <grey.pedrinho@gmail.com>
2019-08-08 14:21:25 +0100

    Adds color animation for time.html

--- webpack/animate-time.js
+++
@@ -1,5 +0,0 @@
-import anime from 'animejs'
-
-anime({
-    targets: '.animate-time'
-})
--- webpack/entry.js
+++ webpack/entry.js
@@ -1,5 +1,5 @@
 import './animateThunder'
-import './animate-time'
+import './animateTime'
 import TableFetcher from './tableFetcher'
 import React from 'react'
 import ReactDOM from 'react-dom'

932d322f60823deb05d0586d1efd9e43e59f4045
Andrew Ijano <andrew.lopes@uol.com.br>
2019-10-09 21:26:50 -0300

    Fixes header bugs and improves code formatting

--- webpack/animateHeader.js
+++ webpack/animateHeader.js
@@ -1,49 +1,59 @@
-import anime from 'animejs'
+import anime from "animejs";
+import color from "./color";
 
-// Detect request animation frame
-var scroll =
+const scroll =
   window.requestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.mozRequestAnimationFrame ||
   window.msRequestAnimationFrame ||
   window.oRequestAnimationFrame ||
-  // IE Fallback, you can even fallback to onscroll
-  function(callback) {
-    window.setTimeout(callback, 1000 / 60);
-  };
-var lastPosition = -1;
+  function() {};
 
-// my Variables
-var lastSection = false;
-var headerItemTop = -1;
-var headerItemBottom = -1;
-var headerItemHeight = -1;
+const colorSchemes = [
+  ["splash", [color.pink, color.white]],
+  ["home--color-scheme-1", [color.yellow, color.pink]],
+  ["home--color-scheme-2", [color.black, color.blue]],
+  ["home--color-scheme-3", [color.blue, color.white]],
+  ["home--color-scheme-4", [color.white, color.pink]],
+  ["home--color-scheme-5", [color.pink, color.white]]
+];
 
-const pinkLH = "#F70D88";
-const yellowLH = "#F9DF25";
-const blueLH = "#01ADEF";
-const whiteLH = "#FFF";
-const blackLH = "#000";
+const oddClasses = colorSchemes
+  .filter(function(el, idx) {
+    return idx % 2 === 1;
+  })
+  .map(function(el) {
+    return el[0];
+  });
 
-// The Scroll Function
+const sections = document.querySelectorAll(".home--section, .splash");
+const headerDynamic = document.querySelectorAll(".header-dynamic");
+const headerItem = document.querySelectorAll(".header-dynamic--item");
+
+let lastPosition = -1;
+let lastSection = false;
+
+let headerItemTop = -1;
+let headerItemBottom = -1;
+let headerItemHeight = -1;
+
+
+function isCurrentSectionOdd(el) {
+  return oddClasses.reduce(function(val, oddClass) {
+    return val || el.classList.contains(oddClass);
+  }, false);
+}
+
 function loop() {
-  if (window.scrollY == 0) {
+  if (window.scrollY === 0) {
     anime({
-      targets: '.header--line',
-      opacity: [0.5,1],
+      targets: ".header--line",
+      opacity: [0.5, 1],
       scaleX: [0, 1],
       easing: "easeInOutExpo",
       duration: 700
-    });    
-
-    anime({
-      targets: ".header-dynamic::after",
-      opacity: 1
     });
   }
-  var sections = document.querySelectorAll(".home--section, .splash");
-  var headerDynamic = document.querySelectorAll(".header-dynamic");
-  var headerItem = document.querySelectorAll(".header-dynamic--item");
 
   if (headerItem.length > 0) {
     // get top position of item from container, because image might not have loaded
@@ -52,96 +62,70 @@ function loop() {
     headerItemBottom = headerItemTop + headerItemHeight;
   }
 
-  var sectionTop = -1;
-  var sectionBottom = -1;
-  var currentSection = -1;
-
-  // Fire when needed
-  if (lastPosition == window.pageYOffset) {
+  if (lastPosition === window.pageYOffset) {
     scroll(loop);
     return false;
-  } else {
-    lastPosition = window.pageYOffset;
-
-    Array.prototype.forEach.call(sections, function(el, i) {
-      sectionTop = parseInt(el.getBoundingClientRect().top);
-      sectionBottom = parseInt(el.getBoundingClientRect().bottom);
-
-      // active section
-      if (sectionTop <= headerItemBottom && sectionBottom > headerItemTop) {
-        // check if current section has bg
-
-        const oddClasses = ["home--color-scheme-1", "home--color-scheme-3", "home--color-scheme-5"];
-
-        currentSection = 
-          el.classList.contains(oddClasses[0]) ||
-          el.classList.contains(oddClasses[1]) ||
-          el.classList.contains(oddClasses[2]);
-
-          const colorSchemes = [ 
-            ["splash", [pinkLH, whiteLH ]], 
-            ["home--color-scheme-1", [yellowLH, pinkLH]], 
-            ["home--color-scheme-2", [blackLH, blueLH]], 
-            ["home--color-scheme-3", [blueLH, whiteLH]], 
-            ["home--color-scheme-4", [whiteLH, pinkLH]], 
-            ["home--color-scheme-5", [pinkLH, whiteLH]]
-          ];
-
-          for (var i in colorSchemes) {
-            const colorScheme = colorSchemes[i];
-            const colorSchemeClass = colorScheme[0];
-            const colors = colorScheme[1];
-            const isOdd = oddClasses.includes(colorSchemeClass);
-
-            const varBackground = "--header-background" + (isOdd ? "-invert" : "");
-            const varTextColor  = "--header-text-color" + (isOdd ? "-invert" : "");
-
-            if (el.classList.contains(colorSchemeClass)) {
-              document.documentElement.style.setProperty(varBackground, colors[0]);
-              document.documentElement.style.setProperty(varTextColor, colors[1]);
-            }
-          }
-
-        // switch class depending on background image
-        if (currentSection)
-          headerDynamic[0].classList.remove("header-dynamic--reverse");
-        else
-          headerDynamic[0].classList.add("header-dynamic--reverse");
-        
-      }
-      // end active section
-
-      // if active Section hits replace area
-      if (headerItemTop < sectionTop && sectionTop <= headerItemBottom) {
-        // animate only, if section background changed
-        if (currentSection != lastSection) {
-          document.documentElement.style.setProperty(
-            "--replace-offset",
-            (100 / headerItemHeight) * parseInt(sectionTop - headerItemTop) +
-              "%"
-          );
-        }
-      }
-
-      // end active section in replace area
-
-      // if section above replace area
-      if (headerItemTop >= sectionTop) {
-        // set offset to 0 if you scroll too fast
-        document.documentElement.style.setProperty("--replace-offset", 0 + "%");
-        // set last section to current section
-        lastSection = currentSection;
-      }
-    });
   }
+  
+  let sectionTop = -1;
+  let sectionBottom = -1;
+  let currentSection = -1;
 
-  // Recall the loop
+  lastPosition = window.pageYOffset;
+
+  sections.forEach((el, i) => {
+    sectionTop = parseInt(el.getBoundingClientRect().top);
+    sectionBottom = parseInt(el.getBoundingClientRect().bottom);
+
+    // active section
+    if (sectionTop <= headerItemBottom && sectionBottom > headerItemTop) {
+      currentSection = isCurrentSectionOdd(el);
+      colorSchemes.forEach(colorScheme => {
+        const [colorSchemeClass, colors] = colorScheme;
+        const isOdd = oddClasses.includes(colorSchemeClass);
+
+        const varBackground = "--header-background" + (isOdd ? "-invert" : "");
+        const varTextColor = "--header-text-color" + (isOdd ? "-invert" : "");
+
+        if (el.classList.contains(colorSchemeClass)) {
+          document.documentElement.style.setProperty(varBackground, colors[0]);
+          document.documentElement.style.setProperty(varTextColor, colors[1]);
+        }
+      });
+
+      // switch class depending on background image
+      if (currentSection) {
+        headerDynamic[0].classList.remove("header-dynamic--reverse");
+      } else {
+        headerDynamic[0].classList.add("header-dynamic--reverse");
+      }
+    }
+
+    // if active section hits replace area
+    if (headerItemTop < sectionTop && sectionTop <= headerItemBottom) {
+      // animate only, if section background changed
+      if (currentSection !== lastSection) {
+        document.documentElement.style.setProperty(
+          "--replace-offset",
+          (100 / headerItemHeight) * parseInt(sectionTop - headerItemTop) + "%"
+        );
+      }
+    }
+
+    // if section above replace area
+    if (headerItemTop >= sectionTop) {
+      // set offset to 0 if you scroll too fast
+      document.documentElement.style.setProperty("--replace-offset", 0 + "%");
+      // set last section to current section
+      lastSection = currentSection;
+    }
+  });
+
   scroll(loop);
 }
 
-// Call the loop for the first time
 loop();
 
-window.onresize = function(event) {
+window.onresize = function() {
   loop();
 };
---
+++ webpack/color.js
@@ -0,0 +1,7 @@
+export default {
+  pink: "#F70D88",
+  yellow: "#F9DF25",
+  blue: "#01ADEF",
+  white: "#FFF",
+  black: "#000"
+};

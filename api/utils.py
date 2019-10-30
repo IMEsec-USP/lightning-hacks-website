dc1b5968a094a65492938d5cb195228e942b7d52
pedro823 <grey.pedrinho@gmail.com>
2019-10-30 13:13:07 -0300

    Adds force refresh if refreshed enough times

--- api/utils.py
+++ api/utils.py
@@ -1,9 +1,12 @@
 from functools import wraps
 from datetime import datetime, timedelta
 
+FORCE_REFRESH_AMOUNT = 5
+
 def cache_response(time: timedelta):
     ''' caches the response until time has passed. '''
     def decorator(f):
+        cache_used_times = 0
         cached_response = None
         cached_until = None
 
@@ -11,10 +14,12 @@ def cache_response(time: timedelta):
         def new_f(*args, **kwargs):
             nonlocal cached_response
             nonlocal cached_until
-            if cached_response is None or cached_until < datetime.utcnow():
+            nonlocal cache_used_times
+            if cached_response is None or cached_until < datetime.utcnow() or cache_used_times > FORCE_REFRESH_AMOUNT:
                 cached_response = f(*args, **kwargs)
                 cached_until = datetime.utcnow() + time
             
+            cache_used_times += 1
             return cached_response
 
         return new_f
